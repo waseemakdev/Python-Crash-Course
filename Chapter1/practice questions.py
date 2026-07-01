2bc3a0550614540595e3118065a703df4c31e4ce
Waseem Akram <74070709+waseemakdev@users.noreply.github.com>
2026-07-01 10:07:37 +0500

    Intial commit

--- Chapter1/practice questions.py
+++ Chapter1/practice questions.py
@@ -1,4 +1,4 @@
 # In this file industry related standard practice questions that know solving the real world problems.
 # Lambda functions practice questions.
 # OOP practices.
-
+# DSA in Python practice question.
